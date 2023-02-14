4cd184ddf12227053c2ad4c95000bd31e712231a
alitemel89 <alitemel89.ce@gmail.com>
2023-02-14 12:15:11 +0300

    update Transactions.jsx

--- src/scenes/transactions/Transactions.jsx
+++ src/scenes/transactions/Transactions.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { mockTransactions } from "../../data/mockData";
 
 const Transactions = () => {
-  console.log(mockTransactions);
   return (
     <div className="w-2/5 h-72 overflow-auto scrollbar-hide">
       <h2 className="text-lg text-white p-2">Recent Transactions</h2>
@@ -17,15 +16,19 @@ const Transactions = () => {
         </thead>
         {mockTransactions.map((transaction, i) => (
           <tbody key={i}>
-            <tr className="text-center bg-slate-700">
+            <tr className="text-center bg-slate-700 shadow-xl">
               <td className="flex flex-col text-gray-200 border-red-700">
                 {transaction.user}{" "}
                 <span className="text-emerald-400">{transaction.txId}</span>
               </td>
-              <td className="border-red-700"> 
-                <div className="p-1 rounded-xl bg-emerald-500 text-white">$ {transaction.cost}</div>
+              <td className="border-red-700">
+                <div className="p-1 rounded-xl bg-emerald-500 text-white">
+                  $ {transaction.cost}
+                </div>
               </td>
-              <td className="text-gray-200 border-red-800">{transaction.date}</td>
+              <td className="text-gray-200 border-red-800">
+                {transaction.date}
+              </td>
             </tr>
           </tbody>
         ))}
